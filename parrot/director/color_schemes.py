3ed673e94e8b36d23cb248a44a4f5069d3c62ede
David Mack <davidhughhenrymack@gmail.com>
2023-12-30 19:38:59 -0800

    kkk

--- parrot/director/color_schemes.py
+++ parrot/director/color_schemes.py
@@ -3,11 +3,11 @@ from parrot.utils.colour import Color
 
 
 color_schemes = [
-    # ColorScheme(Color("green"), Color("blue"), Color("blue")),
-    # ColorScheme(Color("white"), Color("blue"), Color("purple")),
-    # ColorScheme(Color("white"), Color("red"), Color("red")),
-    # ColorScheme(Color("white"), Color("red"), Color("purple")),
+    ColorScheme(Color("green"), Color("blue"), Color("blue")),
+    ColorScheme(Color("white"), Color("blue"), Color("purple")),
+    ColorScheme(Color("white"), Color("red"), Color("red")),
+    ColorScheme(Color("white"), Color("red"), Color("purple")),
     ColorScheme(Color("red"), Color("blue"), Color("blue")),
-    # ColorScheme(Color("magenta"), Color("blue"), Color("purple")),
-    # ColorScheme(Color("blue"), Color("purple"), Color("purple")),
+    ColorScheme(Color("magenta"), Color("blue"), Color("purple")),
+    ColorScheme(Color("blue"), Color("purple"), Color("purple")),
 ]
