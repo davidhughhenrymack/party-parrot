58e53eb85ea8b25ebbbd3b9e665889bc91fe45bb
David Mack <davidhughhenrymack@gmail.com>
2024-06-08 17:12:20 -0700

    moar

--- parrot/fixtures/chauvet/gigbar.py
+++ parrot/fixtures/chauvet/gigbar.py
@@ -1,6 +1,15 @@
 from parrot.fixtures.chauvet.derby import ChauvetDerby
 from parrot.fixtures.chauvet.move9 import ChauvetMove_9Ch
 from parrot.fixtures.chauvet.par import ChauvetParRGBAWU
+from parrot.fixtures.laser import Laser
+
+
+class ChauvetGigbarLaser(Laser):
+    def __init__(self, address):
+        super().__init__(address, "gigbar laser", 1)
+
+    def set_dimmer(self, value):
+        self.values[0] = 6 if value > 0 else 0
 
 
 def ChauvetGigBarMoveILS(address: int):
@@ -9,6 +18,7 @@ def ChauvetGigBarMoveILS(address: int):
         ChauvetParRGBAWU(address + 7),
         ChauvetDerby(address + 14),
         ChauvetDerby(address + 20),
+        ChauvetGigbarLaser(address + 31),
         ChauvetMove_9Ch(address + 32),
         ChauvetMove_9Ch(address + 41),
     ]
