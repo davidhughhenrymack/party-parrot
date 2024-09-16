32e482f6274d01a5d1e14f68209b984813464045
David Mack <davidhughhenrymack@gmail.com>
2024-09-15 18:41:09 -0700

    yay

--- parrot/interpreters/strobe.py
+++ parrot/interpreters/strobe.py
@@ -1,15 +1,18 @@
 from parrot.director.color_scheme import ColorScheme
 from parrot.interpreters.base import InterpreterBase
-from parrot.director.frame import Frame, FrameSignal
+from parrot.director.frame import Frame
+from parrot.fixtures.moving_head import MovingHead
 
 
-class StrobeHighSustained(InterpreterBase):
+class StrobeHighSustained(InterpreterBase[MovingHead]):
     hype = 90
 
     def step(self, frame, scheme):
         for i in self.group:
-            i.set_strobe(50)
+            i.set_strobe(220)
+            i.set_dimmer(255)
 
     def exit(self, frame: Frame, scheme: ColorScheme):
         for i in self.group:
             i.set_strobe(0)
+            i.set_dimmer(0)
